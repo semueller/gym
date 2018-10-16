3107a09051ac9902f40452b1e7925e0ade343905
llach <llach@techfak.uni-bielefeld.de>
2018-10-16 17:28:11 +0200

    ManipulateTouchEnv: added method to change object size

--- gym/envs/robotics/hand/manipulate_touch.py
+++ gym/envs/robotics/hand/manipulate_touch.py
@@ -120,6 +120,13 @@ class ManipulateTouchEnv(manipulate.ManipulateEnv, utils.EzPickle):
             'desired_goal': self.goal.ravel().copy(),
         }
 
+    def set_object_factor(self, factor):
+
+        for name in ['object', 'object_hidden', 'target']:
+            id = self.sim.model._geom_name2id[name]
+
+        self.sim.model.geom_size[id] *= factor
+
 class HandTouchBlockEnv(ManipulateTouchEnv):
     def __init__(self, target_position='random', target_rotation='xyz', reward_type='sparse'):
         super(HandTouchBlockEnv, self).__init__(
